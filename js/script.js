2d000d5903ef16ca4215e7b0fac9b1530eac74b1
Rashik <kumar7rashik@gmail.com>
2017-12-19 20:20:03 +0545

    pushed all file at once so bare with me

---
+++ js/script.js
@@ -0,0 +1,20 @@
+
+//automatically goes to welcome page
+(function() {
+
+	var preload = document.getElementById("preload");
+	var loading = 0;
+	var id = setInterval(frame, 64);
+  	
+  	function frame() {
+  		if(loading == 100) {
+  			clearInterval(id);
+  			window.open("welcome.html","_self");
+  		} else {
+  			loading += 1;
+  			if(loading ==90) {
+  				preload.style.animation = "fadeout 1s ease";
+  			}
+  		}
+  	}
+})();
---
+++ login.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Login</h2>
+        <form action="#" method="post">
+
+                <label>Username:</label>
+                <input type="text" name="username">
+
+                <label>Password:</label>
+                <input type="password" name="password" >
+
+                <input type="submit" value="Submit">
+
+            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+        </form>
+    </div>    
+</body>
+</html>
